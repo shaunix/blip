e15f34f43c9488e00a04d521160a286a74bca130
Shaun McCance <shaunm@gnome.org>
2007-10-05 14:58:07 -0500

    Restoring __all__ to pulse.pulsate with dynamic contents Now pulsate can just look at pulse.pulsate.__all__, and pulse/pulsate/__init__.py will figure out what __all__ should be.

--- pulse/pulsate/__init__.py
+++ pulse/pulsate/__init__.py
@@ -18,3 +18,18 @@
 # Suite 330, Boston, MA  0211-1307  USA.
 #
 
+import os
+import os.path
+import sys
+
+__all__ = []
+
+for f in os.listdir (os.path.dirname (sys.modules[__name__].__file__)):
+    if f.endswith ('.py'):
+        tool = os.path.basename (f)[:-3]
+    elif f.endswith ('.pyc'):
+        tool = os.path.basename (f)[:-4]
+    else:
+        continue
+    if not tool in __all__:
+        __all__.append (tool)
