91d92a0b98818dc587c24bd1f7ea3c9c5e42cfd8
Shaun McCance <shaunm@gnome.org>
2009-03-17 16:20:43 -0500

    Added --time-limit to queue.py

--- pulse/pulsate/queue.py
+++ pulse/pulsate/queue.py
@@ -26,6 +26,7 @@ usage_extra = ''
 args = pulse.utils.odict()
 args['length'] = (None, 'print the length of the queue and exit')
 args['limit='] = ('num', 'process at most num entries from the queue')
+args['time-limit='] = ('time', 'process the queue for at most time seconds')
 args['no-history'] = (None, 'do not check SCM history')
 args['no-timestamps'] = (None, 'do not check timestamps before processing files')
 args['no-update']  = (None, 'do not update SCM checkouts')
@@ -41,12 +42,32 @@ def main (argv, options={}):
         print pulse.db.Queue.select().count()
         return 0
     limit = options.get ('--limit', None)
-    if limit != None:
+    if limit is not None:
         limit = int(limit)
+    timelimit = options.get ('--time-limit', None)
+    if timelimit is not None:
+        sep = timelimit.rfind (':')
+        tlhour = tlmin = tlsec = 0
+        if sep >= 0:
+            tlsec = int(timelimit[sep+1:])
+            tlpre = timelimit[:sep]
+            sep = tlpre.rfind (':')
+            if sep >= 0:
+                tlmin = int(tlpre[sep+1:])
+                tlhour = int(tlpre[:sep])
+            else:
+                tlmin = int(tlpre)
+        else:
+            tlsec = int(timelimit)
+        timelimit = 3600 * tlhour + 60 * tlmin + tlsec
+        import datetime
+        timestart = datetime.datetime.now()
     iter = 0
     el = pulse.db.Queue.pop ()
     while el != None:
         if limit != None and iter >= limit:
+            return 0
+        if timelimit != None and (datetime.datetime.now() - timestart).seconds > timelimit:
             return 0
         iter += 1
         mod = pulse.utils.import_ ('pulse.pulsate.' + el['module'])
